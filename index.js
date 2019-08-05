b985bc1a6776a888c07bf19da5d0bbd1d2e2022c
giladbarnea <giladbrn@gmail.com>
2019-08-05 04:54:14 +0300

    resume

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const body = elem({ htmlElement: document.body });
 const resumePageLink = elem({ id: 'resume_page_link' });
 resumePageLink.pointerdown(() => {
-    body.empty();
+    window.location.assign('/resume');
 });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const body = elem({htmlElement: document.body});
 const resumePageLink = elem({id: 'resume_page_link'});
 resumePageLink.pointerdown(() => {
-    body.empty();
+    window.location.assign('/resume')
 });
 // resumePageLink.addEventListener('click', () => {
 //     console.log('clicked resumePageLink');
