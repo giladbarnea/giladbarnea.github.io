12d6ee4815daa2426b8bc09575746c6c8ddf03a4
giladbarnea <giladbrn@gmail.com>
2019-08-05 04:56:43 +0300

    testing resume/index.html without ---...---

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const body = elem({ htmlElement: document.body });
 const resumePageLink = elem({ id: 'resume_page_link' });
 resumePageLink.pointerdown(() => {
-    window.location.assign('/resume');
+    window.location.assign('resume');
 });
 //# sourceMappingURL=index.js.map
