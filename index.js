d55c765024f889b569559e83b2e01756234fac9f
giladbarnea <giladbrn@gmail.com>
2019-08-05 04:19:48 +0300

    resumePageLink click body.empty()

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const resumePageLink = document.getElementById('resume_page_link');
-resumePageLink.addEventListener('click', () => {
-    console.log('clicked resumePageLink');
+const body = elem({ htmlElement: document.body });
+const resumePageLink = elem({ id: 'resume_page_link' });
+resumePageLink.pointerdown(() => {
+    body.empty();
 });
 //# sourceMappingURL=index.js.map
